0cafaee00728eae868328e1b08adcfdef2dc7d31
Om Choudhary <130913798+0mchoudhary@users.noreply.github.com>
2025-04-04 12:29:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_r89vpASGW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '9i77po2rhmn86hdugiq2e9r38', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: '19pq9iserl7a6jqdnu88coe3ft250nho5d2rjn8baqu6b6u9kh0j'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
