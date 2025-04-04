d30b869b8c980d0efd527f6a91000a1ad5ccb9dc
Om Choudhary <130913798+0mchoudhary@users.noreply.github.com>
2025-04-04 11:36:20 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_r89vpASGW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9i77po2rhmn86hdugiq2e9r38', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
